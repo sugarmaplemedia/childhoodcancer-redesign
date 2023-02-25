d0d17916a0884f7baae4c30ff54b725ce4c3021a
Harrison Bouche <harrison@sm.media>
2023-02-25 17:36:12 -0500

    Added assets, menu, hero, icon framework

---
+++ src/components/home/StatsDisplay.svelte
@@ -0,0 +1,10 @@
+<section class="
+    w-5/6 max-w-4xl h-20 m-auto
+    relative z-10
+    bg-navy-super-light rounded-3xl">
+    <div class="
+        flex flex-col items justify-center">
+        
+    </div>
+
+</section>
