8bf2a6ed89206782e371dd7c957fbea820caeef6
Harrison Bouche <harrison@sm.media>
2023-04-11 11:07:34 -0400

    Hero complete, formatted

--- src/components/home/Hero.svelte
+++ src/components/home/Hero.svelte
@@ -1,31 +1,33 @@
-<script>
-    import Heading from "@components/Heading.svelte";
-    import Button from "@components/Button.svelte";
+<script lang="ts">
+    import Button from "@components/Button.svelte"
+
 </script>
 
 <section class="
-    relative
-
-    before:content-['']
-    before:w-full before:h-full
-    before:absolute before:top-0 before:left-0 before:-z-20
-    before:bg-[url('/images/landing-hero.jpg')] before:bg-cover before:bg-center
-    before:brightness-65 before:saturate-75
-    ">
+        w-full mb-16
+        relative
+        
+        before:w-full before:h-full
+        before:absolute before:top-0 before:left-0 before:-z-20
+        before:bg-[url('{import.meta.env.BASE_URL}/images/landing-hero.jpg')] before:bg-cover before:bg-left sm:before:bg-center
+        before:brightness-65 before:saturate-75">
     <div class="
-        max-w-xl ml-24 pt-24 pb-48
-        relative">
-        <Heading size={1} color="white">
-            Working every day<br />
-            <span class="
-                block w-96 py-2
-                text-2xl text-white">
-                to fundraise for U.P. families battling <span class="text-orange">childhood cancer.</span>
-            </span>
-        </Heading>
-        <p class="
-            pt-2 pb-6
-            font-sans font-medium text-white">Since our project began, we have seen a rise in the number of families living in the Upper Peninsula of Michigan seeking help with a child battling cancer. As of December 2022, that number has increased by more <span class="font-bold text-orange">than twenty-fold!</span></p>
-        <Button>Join the fight</Button>
+            max-w-lg lg:max-w-3xl px-8 pt-24 pb-[30vw] xl:pb-48 mx-auto xl:ml-24
+            flex flex-col items-center lg:items-start gap-4
+            text-center lg:text-left
+            relative">
+        <h1 class="text-white">
+            Helping Upper Peninsula families <span class="text-orange">fight</span> childhood cancer.
+        </h1>
+        <p class=" text-white">Since our project began, we have seen a rise in the number of families living in the Upper Peninsula of Michigan seeking help with a child battling cancer.</p>
+        <p class="mb-2 text-lg font-bold text-white">As of December 2022, that number has increased by more <span class="text-orange">than twenty-fold!</span></p>
+        <Button href="{import.meta.env.BASE_URL}/donate">Join the fight</Button>
+    </div>
+    <div class="
+            w-full xl:min-w-[1600px] -mt-[15rem]
+            absolute bottom-24 xl:bottom-auto z-10 overflow-x-hidden 
+            translate-y-32 xl:translate-y-0 scale-[3] sm:scale-[2] xl:scale-110 -rotate-9 -left-8
+            pointer-events-none">
+        <img src="{import.meta.env.BASE_URL}/graphics/swooshes/Asset 2.svg" alt="" class="select-none" />
     </div>
 </section>
